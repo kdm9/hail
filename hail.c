e05793f0d23a62232196b0e24391e1dae2f80ef0
Kevin Murray <spam@kdmurray.id.au>
2015-02-17 16:31:51 +1100

    Better comments in hail.c
    Also added GPL heading comment

--- hail.c
+++ hail.c
@@ -1,4 +1,21 @@
 /*
+ * Copyright 2015 Kevin Murray
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * ============================================================================
  *
  *       Filename:  hail.c
@@ -8,12 +25,17 @@
  *
  * ============================================================================
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 
-#define usage() \
-        printf("Usage: %s <first>-<last> [<first>-<last ...]\n", argv[0])
+
+/* Defined as a macro to allow use of argv[0]. */
+#define usage()                                                             \
+        fprintf(stderr, "Usage: %s <first>-<last> [<first>-<last ...]\n",   \
+                argv[0])
+
 int
 main(int argc, const char **argv)
 {
@@ -25,22 +47,29 @@ main(int argc, const char **argv)
     size_t first, last;
     int i = 0;
 
+    /* We expect to have at least one pair of start-finish line nums. */
     if (argc < 2) {
         usage();
         return(EXIT_FAILURE);
     }
 
+    /* For each pair of <start>-<finish> */
     for (i = 1; i < argc; i++) {
+        /* Parse the <x>-<y> from argv, storing how many elements we parsed. */
         res = sscanf(argv[i], "%zu-%zu", &first, &last);
+        /* we expect at least one value, so error out if we don't have one. */
         if (res < 1) {
-            /* we expect at least one value */
             putchar('\n');
             usage();
-            break;
+            fflush(stdout);
+            free(buf);
+            return(EXIT_FAILURE);
         }
+        /* We have something like 2-, so we go till the last line. */
         if (res == 1) {
             last = SIZE_MAX;
         }
+        /* Main brains of hail: Grab line and print it if it's in the range. */
         while (lineno < last) {
             if (getline(&buf, &len, fp) < 1) {
                 /* EOF or error */
@@ -52,6 +81,7 @@ main(int argc, const char **argv)
             }
         }
     }
+    /* Clean up and exit nicely */
     fflush(stdout);
     free(buf);
     return(EXIT_SUCCESS);
