56ed5e976cd10a1483533cdcf941e61cd76b877c
Kevin Murray <spam@kdmurray.id.au>
2015-07-12 15:49:31 +1000

    Minor changes to hail.c
    remove extra parens around returns, remove duplicated license header

--- hail.c
+++ hail.c
@@ -1,21 +1,4 @@
 /*
- * Copyright 2015 Kevin Murray
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
  * ============================================================================
  *
  *       Filename:  hail.c
@@ -50,7 +33,7 @@ main(int argc, const char **argv)
     /* We expect to have at least one pair of start-finish line nums. */
     if (argc < 2) {
         usage();
-        return(EXIT_FAILURE);
+        return EXIT_FAILURE ;
     }
 
     /* For each pair of <start>-<finish> */
@@ -63,7 +46,7 @@ main(int argc, const char **argv)
             usage();
             fflush(stdout);
             free(buf);
-            return(EXIT_FAILURE);
+            return EXIT_FAILURE ;
         }
         /* We have something like 2-, so we go till the last line. */
         if (res == 1) {
@@ -75,8 +58,7 @@ main(int argc, const char **argv)
                 /* EOF or error */
                 break;
             }
-            lineno++;
-            if (lineno >= first) {
+            if (++lineno >= first) {
                 fputs(buf, stdout);
             }
         }
@@ -84,5 +66,5 @@ main(int argc, const char **argv)
     /* Clean up and exit nicely */
     fflush(stdout);
     free(buf);
-    return(EXIT_SUCCESS);
+    return EXIT_SUCCESS ;
 }
